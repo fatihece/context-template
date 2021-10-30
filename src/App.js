dd0b401cd08799fb4610f831c3cc947e7f694ead
fatih <fatihenaz@gmail.com>
2021-10-30 19:48:44 +0300

    completed theme change action

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import Footer from './Footer'
 import Header from './Header'
 
 const App = () => {
-    const[theme, setTheme] = React.useState("light")
+    const [theme, setTheme] = React.useState("light")
+    
+    React.useEffect(() => {
+        document.body.className = theme
+    }, [theme])
+    
     return (
         <div>
             <h2 style={{ color: "red" }}>Learning context </h2>
