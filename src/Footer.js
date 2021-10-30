dc840ff208f6226c2da4b35a307b2acf4d61dbba
fatih <fatihenaz@gmail.com>
2021-10-30 20:25:35 +0300

    themeSwitcher created as a extract component

--- src/Footer.js
+++ src/Footer.js
@@ -1,18 +1,15 @@
 import React from 'react'
-import {MainContext, useContext} from "./context"
+import ThemeSwitcher from './ThemeSwitcher'
 
 
 
 const Footer = () => {
-    const changeTheme = () => {
-       setTheme(theme === "light" ? "dark" : "light")
-    }
+  
     
-    const {setTheme, theme} = useContext(MainContext)
     return (
         <div>
             <h2>Footer Component</h2>
-             <button onClick={changeTheme}>Switch theme</button>
+            <ThemeSwitcher />
         </div>
     )
 }
